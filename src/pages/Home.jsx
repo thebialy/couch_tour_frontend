5100ba69a34a3336b95b8b536837489c403cb849
thebialy <67696052+thebialy@users.noreply.github.com>
2020-12-11 19:59:50 -0600

    last commit or something?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Home = (props) => {
-    return <h2> Home</h2>
+    return <h1> Welcome to Couch Tour. An application to plan your very own virtual music tour. Signup and login to start adding your favorite bands' livestreams to begin your couch tour!</h1>
 }
 
 export default Home
